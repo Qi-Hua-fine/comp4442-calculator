4639b9e837e51eafa5bc1bc2463aaea138000341
FANG Keshen <helloworld>
2026-04-09 04:38:45 +0800

    init project

--- src/main/java/com/example/calculator_service/CalculatorApplication.java
+++ src/main/java/com/example/calculator_service/CalculatorApplication.java
@@ -2,8 +2,6 @@ package com.example.calculator_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,14 +11,5 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
-	}
-
-	// 加法
-	@GetMapping("/add")
-	public String add(
-			@RequestParam int a,
-			@RequestParam int b
-	) {
-		return a + " + " + b + " = " + (a + b);
 	}
 }
