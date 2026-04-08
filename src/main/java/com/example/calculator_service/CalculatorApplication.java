be2e6f77da83e5090bf1f7dd1ac91455fbef9cfe
FANG Keshen <helloworld>
2026-04-09 06:08:35 +0800

    新加功能：除法 平方 多数字求和

--- src/main/java/com/example/calculator_service/CalculatorApplication.java
+++ src/main/java/com/example/calculator_service/CalculatorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class CalculatorApplication {
@@ -42,4 +44,34 @@ public class CalculatorApplication {
 	}
 
 	//   my work finish：FANG Keshen
+
+	// 4. 除法
+	@GetMapping("/div")
+	public String div(
+			@RequestParam double a,
+			@RequestParam double b
+	) {
+		if (b == 0) {
+			return "错误：除数不能为 0";
+		}
+		return a + " ÷ " + b + " = " + (a / b);
+	}
+
+	// 5. 平方
+	@GetMapping("/square")
+	public String square(
+			@RequestParam int a
+	) {
+		return a + " 的平方 = " + (a * a);
+	}
+
+	// 6. 多个数字求和
+	@GetMapping("/sum")
+	public String sum(@RequestParam List<Integer> numbers) {
+		int total = 0;
+		for (int num : numbers) {
+			total += num;
+		}
+		return "输入数字总和 = " + total;
+	}
 }
