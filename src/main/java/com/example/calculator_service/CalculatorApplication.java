81f6851886593fce70713bfb7d611ce0fd86d028
FANG Keshen <helloworld>
2026-04-09 04:40:33 +0800

    add add api

--- src/main/java/com/example/calculator_service/CalculatorApplication.java
+++ src/main/java/com/example/calculator_service/CalculatorApplication.java
@@ -3,7 +3,8 @@ package com.example.calculator_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
@@ -11,5 +12,14 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+	// 加法
+	@GetMapping("/add")
+	public String add(
+			@RequestParam int a,
+			@RequestParam int b
+	) {
+		return a + " + " + b + " = " + (a + b);
 	}
 }
